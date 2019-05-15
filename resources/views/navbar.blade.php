928e2c22e9372c541c75c5b2690f67c5f8b998db
hugobrando <44117636+hugobrando@users.noreply.github.com>
2019-05-15 20:40:14 +0200

    navbar responsive

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -16,46 +16,66 @@
 
 <body>
 	<div class="container-fluid">
-
-		<nav class="navbar navbar-expand-sm navbar-dark bg-primary fond ">
+		<nav class="navbar navbar-expand-xl navbar-dark bg-primary fond ">
 			<a class="navbar-brand" href="#">
 				<img src="image/logo.png" alt="Le logo du site" class="taille">
 			</a>
 
 			<a class="navbar-brand" href="#"> 
-				<h1>"Nom de l'entreprise"</h1>
+				<h1 class="titre">"Nom de l'entreprise"</h1>
 			</a>
-		   
-			<div class="dropdown">
-			  <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    Commande
-			  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
 
-			  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			    <a class="dropdown-item" href="./order">Commande à passer</a>
-			    <a class="dropdown-item" href="./orderHistory">Commande en cours/terminée</a>
-			    <a class="dropdown-item" href="./ignoreProduct">Produit désactivé</a>
-			  </div>
-			</div>
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
 
-			<div class="dropdown">
-			  <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			    Ajouter/Modifier
-			  </a>
 
-			  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			    <a class="dropdown-item" href="./createProduct">Un produit</a>
-			    <a class="dropdown-item" href="./createUser">Un compte</a>
-			    <a class="dropdown-item" href="./editCategory">Une categorie</a>
-			  </div>
-			</div>
-		  	
-		    <button class="btn btn-primary" type="button"><a href="./saleHistory" class="deco">Historique des ventes</a></button>
+		      <li class="nav-item active">
+				<div class="dropdown">
+				  <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Commande
+				  </a>
 
-			<form class="form-inline my-2 my-lg-0 nav">
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+				    <a class="dropdown-item" href="./order">Commande à passer</a>
+				    <a class="dropdown-item" href="./orderHistory">Commande en cours/terminée</a>
+				    <a class="dropdown-item" href="./ignoreProduct">Produit désactivé</a>
+				  </div>
+				</div>
+		      </li>
+
+
+		      <li class="nav-item">
+		        <div class="dropdown">
+				  <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Ajouter/Modifier
+				  </a>
+
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+				    <a class="dropdown-item" href="./createProduct">Un produit</a>
+				    <a class="dropdown-item" href="./createUser">Un compte</a>
+				    <a class="dropdown-item" href="./editCategory">Une categorie</a>
+				  </div>
+				</div>
+		      </li>
+
+
+		      <li class="nav-item dropdown">
+		        <button class="btn btn-primary" type="button"><a href="./saleHistory" class="deco">Historique des ventes</a></button>
+		      </li>
+
+
+		      
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0 nav">
 			    <button class="btn btn-success position" type="submit"><a href="./deconnect" class="deco">Déconnexion</a></button>
 			</form>
+		  </div>
 		</nav>
+
+
 	</div>
 
 
