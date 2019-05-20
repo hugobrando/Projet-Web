ceeb89455e605b247931254d122861d1ddd9d607
hugobrando <44117636+hugobrando@users.noreply.github.com>
2019-05-20 21:38:55 +0200

    rectif deco + https only

--- app/Http/Controllers/ConnexionControler.php
+++ app/Http/Controllers/ConnexionControler.php
@@ -9,14 +9,19 @@ use App\Boss;
 class ConnexionControler extends Controller
 {
     
-    public function deconnect(){
+    public function deconnect(Request $request){
         if(auth()->guard('user')->check()){ //si l'utilisateur est connecté en tant que user 
         
         auth()->guard('user')->logout();
+        $request->session()->flush(); //efface toutes les informations
+        $request->session()->regenerate(); // modifie l'identifiant de session
+
        }
        else{ // sin non c'est un boos
 
         auth()->guard('boss')->logout();
+        $request->session()->flush(); //efface toutes les informations
+        $request->session()->regenerate(); // modifie l'identifiant de session
        } 
 
         return redirect('/')->with('deconnect', 'Vous avez été déconnecté !');
